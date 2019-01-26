c2e3c4caa373e91e3f5a3217a76b81ff59e481a3
zhuruisong <ruisongzhu@huashenghaoche.com>
2019-01-26 22:52:28 +0800

    init

--- rpc-project/consumer/src/main/java/com/zrs/rpc/consumer/Test.java
+++ rpc-project/consumer/src/main/java/com/zrs/rpc/consumer/Test.java
@@ -1,10 +1,21 @@
 package com.zrs.rpc.consumer;
 
+import org.springframework.core.DefaultParameterNameDiscoverer;
+import org.springframework.core.ParameterNameDiscoverer;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.lang.reflect.Method;
+import java.util.stream.Stream;
+
+
 public interface Test {
 
-    public String  say(String message);
+    public String  say(@RequestParam String message);
 
-    public static void main(String[] args) {
-
+    public static void main(String[] args) throws NoSuchMethodException {
+        Method say = Test.class.getMethod("say",String.class);
+        ParameterNameDiscoverer parameterNameDiscoverer = new DefaultParameterNameDiscoverer();
+        String[] parameterNames =parameterNameDiscoverer.getParameterNames(say);
+        Stream.of(parameterNames).forEach(System.out::println);
     }
 }
