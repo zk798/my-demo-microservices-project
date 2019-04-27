87f9f85a919eb4aa772046d1559e5660eded294a
github <zhusong2010@183.com>
2019-04-27 08:59:41 +0800

    。。。

--- arithmetic-project/src/main/java/com/zrs/stack/Node.java
+++ arithmetic-project/src/main/java/com/zrs/stack/Node.java
@@ -14,4 +14,24 @@ public class Node <T>{
     public Node() {
     }
 
+
+    public static void main(String[] args) {
+        Node<Integer> node = new Node<>(1);
+        Node<Integer> node4 = new Node<>(4);
+        Node<Integer> node5 = new Node<>(5);
+        Node<Integer> node7 = new Node<>(7);
+        Node<Integer> node8 = new Node<>(8);
+        node.setNext(node4);
+        node4.setNext(node5);
+        node5.setNext(node7);
+        node7.setNext(node8);
+
+        while(node !=null){
+            System.out.println(node.value);
+            node = node.getNext();
+        }
+
+    }
+
+
 }
